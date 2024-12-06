716bf8dd4b3cdd166eb38f1b4507a5da434931db
luka chikashvili <lukachikashvili2020@gmail.com>
2024-12-07 01:34:30 +0400

    add shader effect

--- fitness/src/components/CanvasPage.jsx
+++ fitness/src/components/CanvasPage.jsx
@@ -1,4 +1,4 @@
-import {Canvas } from '@react-three/fiber'
+import {Canvas, useFrame } from '@react-three/fiber'
 import { OrbitControls, Text, Text3D, useGLTF} from "@react-three/drei"
 import Lights from './Lights';
 import { Physics, RigidBody } from '@react-three/rapier'
@@ -6,92 +6,17 @@ import { useRef } from 'react';
 import * as THREE from 'three'
 import vertex from '../shaders/plane/vertex.glsl'
 import fragment from '../shaders/plane/fragment.glsl'
+import Experience from './Experience';
 
 const CanvasPage = () => {
-    const model = useGLTF('./dumb.glb ');
 
-    const uniforms = useRef({
-        color1: { value: new THREE.Color('#474F7A')},
-        color2: { value: new THREE.Color('#98ABEE')}
-      });
-  
 
   return (
     <>
       <Canvas camera={{position: [10, 15.2, 4],   fov: 75, near: 0.1, far: 1000 }}>
     <Physics >
         <Lights />
-        <OrbitControls makeDefault />
-
-<RigidBody type='fixed'>
-        <mesh rotation={[-Math.PI / 2, 0, 0]} position={[-6, 0, -5]} receiveShadow>
-        <planeGeometry args={[150, 150]} />
-        <shaderMaterial uniforms={uniforms.current} vertexShader={vertex} fragmentShader={fragment} />
-      </mesh>
-      </RigidBody>
-
-     <RigidBody colliders = "hull" restitution={1}>
-       <primitive
-        object={model.scene}
-        scale={1}
-        castShadow
-        rotation={[0, 0.3, 0]}
-        position={[4, 20, -5]}  
-      />
-</RigidBody>
-
-<RigidBody colliders = "hull" restitution={1}>
-       <primitive
-        object={model.scene.clone()}
-        scale={1}
-        castShadow
-        rotation={[0, 0.3, 0]}
-        position={[4, 30, -2]}  
-      />
-</RigidBody>
-
-<RigidBody colliders = "hull" restitution={1}>
-       <primitive
-        object={model.scene.clone()}
-        scale={1}
-        castShadow
-        rotation={[0, 0.3, 0]}
-        position={[4, 40, -8]}  
-      />
-</RigidBody>
-
-<Text3D font='./helvetiker_regular.typeface.json'
- size={ 5 }
- height={ 0.2 }
- curveSegments={ 12 }
- bevelEnabled
- bevelThickness={ 0.4 }
- bevelSize={ 0.02 }
- bevelOffset={ 0 }
- bevelSegments={ 5 }
- rotation = {[-Math.PI * 0.5, 0, 1.2]}
- position = {[-8, 0, 17]}
->
-  Baia Fitness
-  <meshStandardMaterial color="orange" />
-</Text3D>
-
-<Text3D font='./helvetiker_regular.typeface.json'
- size={ 1 }
- height={ 0.2 }
- curveSegments={ 12 }
- bevelEnabled
- bevelThickness={ 0.02 }
- bevelSize={ 0.02 }
- bevelOffset={ 0 }
- bevelSegments={ 5 }
- rotation = {[-Math.PI * 0.5, 0, 1.2]}
- position = {[-4, 0, 15]}
->
-Your Journey to Strength and Wellness Begins Here.
-</Text3D>
-
-
+        <Experience />
 </Physics>
       </Canvas>
     </>
--- fitness/src/shaders/plane/fragment.glsl
+++ fitness/src/shaders/plane/fragment.glsl
@@ -1,15 +1,14 @@
- varying vec2 vUv; 
-      uniform vec3 color1;
-      uniform vec3 color2; 
- 
+uniform vec2 rippleCenter;
+uniform float time;
+uniform float rippleIntensity;
+uniform vec3 color1;
+uniform vec3 color2;
 
+varying vec2 vUv;
 
 void main() {
-     
-   
-
-     vec3 gradient = mix(color2, color1, vUv.y );
-
-
-      gl_FragColor = vec4(gradient, 1.0);
+    float dist = distance(vUv, rippleCenter);
+    float ripple = sin((dist - time) * 20.0) * rippleIntensity / (dist + 1.0);
+    vec3 color = mix(color1, color2, vUv.y + ripple);
+    gl_FragColor = vec4(color, 1.0);
 }
