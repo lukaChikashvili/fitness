e51402e0f510de8019ece8df7a10367c47964bec
luka chikashvili <lukachikashvili2020@gmail.com>
2024-12-07 15:58:42 +0400

    removed three js

--- fitness/src/App.jsx
+++ fitness/src/App.jsx
@@ -1,5 +1,5 @@
 import About from "./components/About"
-import CanvasPage from "./components/CanvasPage"
+
 import Header from "./components/Header"
 import Hero from "./components/Hero"
 
@@ -8,17 +8,9 @@ function App() {
 
   return (
     <>
-   <div className="app-container">
       <Header />
-      <section id="hero">
-        <Hero />
-      </section>
-      
-      <section id="about">
-        <About />
-      </section>
-    </div>
-     
+      <Hero />
+      <About />
     </>
   )
 }
--- fitness/src/components/Hero.jsx
+++ fitness/src/components/Hero.jsx
@@ -1,13 +1,11 @@
-import CanvasPage from "./CanvasPage"
+
 
 
 const Hero = () => {
   return (
-    <div className='text-white  w-full h-screen absolute top-0 left-0'>
-    
-    <CanvasPage />
-
-    </div>
+   <div className="w-full min-h-screen ">
+     hero
+  </div>
   )
 }
 
