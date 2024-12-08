8577e16bd23e4762dfc87d90e903c16c2874c31b
luka chikashvili <lukachikashvili2020@gmail.com>
2024-12-08 10:36:17 +0400

    add text reveal with framer motion

--- fitness/src/components/Hero.jsx
+++ fitness/src/components/Hero.jsx
@@ -2,6 +2,7 @@
 import { useContext } from "react";
 import video from "../assets/video.mp4";
 import { UserContext } from "../context/UserContext";
+import { motion } from 'framer-motion'
 
 const Hero = () => {
   
@@ -16,9 +17,16 @@ const Hero = () => {
       <div className="absolute top-0 left-0 w-full h-full bg-black" style={{ opacity: 0.4 }}></div>
       <div className="relative z-10 text-center text-white p-8 flex  items-end justify-start h-screen -mt-[6rem] ml-[10rem]">
          <div className="flex flex-col gap-6">
-           <h1 className="text-6xl font-bold">{geo ? "ბაია ფიტნესი " : "Welcome to baia"}</h1>
-           <span className="w-full h-[0.2px] bg-white" />
-           <p>Your Journey to Strength and Wellness Begins Here</p>
+           <motion.h1 initial = {{opacity: 0,  clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0 100%)"}}
+                      whileInView={{opacity: 1, clipPath:  "polygon(0 0%, 100% 0%, 100% 100%, 0 100%)"}}
+                      transition={{duration: 0.8, ease: 'easeIn', delay: 1}}
+            className="text-7xl font-bold">{geo ? "ბაია ფიტნესი " : "Welcome to baia"}</motion.h1>
+           <motion.span initial = {{width: 0, opacity: 0}} whileInView={{width: '100%', opacity: 1}}
+             transition={{duration: 1, ease: 'easeIn', delay: 1.2}}
+            className="w-full h-[0.2px] bg-white" />
+           <motion.p initial = {{opacity: 0,  clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0 100%)"}}
+                      whileInView={{opacity: 1, clipPath:  "polygon(0 0%, 100% 0%, 100% 100%, 0 100%)"}}
+                      transition={{duration: 0.8, ease: 'easeIn', delay: 1.3}} className="text-xl">Your Journey to Strength and Wellness Begins Here</motion.p>
          </div>
         
       </div>
